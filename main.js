6399067e75e963ae43bf51b2fcd900b7d697104d
Yan Hong <Onlyhongyan@gmail.com>
2016-10-05 17:53:03 -0700

    update footer example, add error handling

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ var path = require('path');
 var getDirName = require('path').dirname;
 var regExp = /\{\s?'([^'\s?}]+)\'\s?}/g;
 
-console.log('Importing from ' + process.argv[2].yellow);
-
 function fromDir(startPath,filter,callback){
 
     if (!fs.existsSync(startPath)){
@@ -34,27 +32,32 @@ function writeFile(path, contents, cb) {
   });
 }
 
-fromDir(process.argv[2],/\.html$/,function(filename){
-      fs.readFile(filename, 'utf8', function(err, contents) {
-      var matches = [];
-      if (err) {
-        return console.log(err.red);
-      }
-      var match = regExp.exec(contents);
-      while (match != null) {
-          matches.push(match);
-          match = regExp.exec(contents);
-      }
-      for (var i = 0; i < matches.length; i++) {
-        var match = matches[i];
-        var patialContent = fs.readFileSync(process.argv[2].replace(/\/?$/, '/') + match[1], 'utf8');
-          contents = contents.replace(match[0], patialContent);
-          console.log('Replaced url ' + match[1].cyan);
-      }
-      var exportPath = filename.replace(process.argv[2], process.argv[3]);
-      console.log('Exporting to ' + exportPath.yellow);
-      writeFile(exportPath, contents);
-    });
-});
+if (process.argv[2] && process.argv[2]) {
+  console.log('Importing from ' + process.argv[2].yellow);
 
-console.log('Juice Packing...');
+  fromDir(process.argv[2],/\.html$/,function(filename){
+        fs.readFile(filename, 'utf8', function(err, contents) {
+        var matches = [];
+        if (err) {
+          return console.log(err.red);
+        }
+        var match = regExp.exec(contents);
+        while (match != null) {
+            matches.push(match);
+            match = regExp.exec(contents);
+        }
+        for (var i = 0; i < matches.length; i++) {
+          var match = matches[i];
+          var patialContent = fs.readFileSync(process.argv[2].replace(/\/?$/, '/') + match[1], 'utf8');
+            contents = contents.replace(match[0], patialContent);
+            console.log('Replaced url ' + match[1].cyan);
+        }
+        var exportPath = filename.replace(process.argv[2], process.argv[3]);
+        console.log('Exporting to ' + exportPath.yellow);
+        writeFile(exportPath, contents);
+      });
+  });
+} else {
+  console.log('Please run juice-pack with import and export directories, for example: "juice-pack templates/ exports/"'.red);
+}
+
